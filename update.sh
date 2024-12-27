9f4cc5fb55dd0d9a1b82d24dd3d9ea208b789a5c
Michal Sojka <michal.sojka@cvut.cz>
2024-12-27 21:06:48 +0100

    update.sh: Reapply our patches after every update

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 for url in \
     https://github.com/autowarefoundation/autoware.git \
     https://github.com/astuff/pacmod3_msgs.git
@@ -14,10 +16,15 @@ done
 
 pushd autoware
 mkdir -p src
+# Tag previous version
+vcs custom src --git --args tag --force autoware.nix
+# Update repositories
 vcs import src < autoware.repos
+# Reapply custom patches (cherry-pick fails on empty commit ranges so we have to ignore all errors)
+vcs custom src --git --args cherry-pick HEAD..autoware.nix || :
 popd
 
 mkdir -p pkgs
 rm pkgs/*
 # shellcheck disable=SC2046
-ros2nix --output-as-nix-pkg-name --output-dir pkgs --fetch $(find . -name package.xml|grep -v ament_cmake)
+ros2nix --output-as-nix-pkg-name --output-dir pkgs --fetch --patches $(find . -name package.xml|grep -v ament_cmake)
